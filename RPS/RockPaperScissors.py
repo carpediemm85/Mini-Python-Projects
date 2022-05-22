3a1fb0e6e30b02373fb06a06f360d8b7e487731b
Rustam Abbaszada <71736644+carpediemm85@users.noreply.github.com>
2022-05-22 17:55:17 +0000

    Rock-paper-scissors

---
+++ RPS/RockPaperScissors.py
@@ -0,0 +1,32 @@
+import random
+import re 
+from secrets import choice
+
+while True:
+    print("\n")
+    print("------Rock Paper Scissors ------")
+    userChoice = input("Choose your weapon : [R]ock, [P]aper, [S]cissors !  >>> ")
+    if not re.match("[SsRrPp]", userChoice):
+        print ("Please choose a letter:")
+        print ("[R]ock, [S]cissors or [P]aper.")
+        continue
+    print("\n") 
+    print("Your choise: " + userChoice)
+    choices = ("P", "R", "S")
+    opponentChoice =random.choice(choices)
+    print("I chose: " + opponentChoice)
+
+    if opponentChoice == userChoice:
+        print("Thie! ")
+
+    elif opponentChoice == 'R' and userChoice == 'S':
+        print ("Scissors beats rock, I win! ")
+        continue
+    elif opponentChoice == 'S' and userChoice.upper() == 'P':      
+        print ("Scissors beats paper! I win! ")
+        continue
+    elif opponentChoice == 'P' and userChoice.upper() == 'R':      
+        print ("Paper beat rock, I win! ")
+        continue
+    else:       
+        print ("----You win!----")
